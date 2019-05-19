b596fcf77b8932b33ed0c8c9ca76e9f658af1c5b
Alexis K Grisham <alexisgrisham@AlexisGMac.lan1>
2019-05-19 17:48:04 -0400

    addedkata

--- src/App.js
+++ src/App.js
@@ -15,6 +15,36 @@ function getCount(str) {
   
   return vowelsCount;
 }
+
+function isPangram(string){
+  this.string = string.toLowerCase();
+  
+    
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  
+      var sentence = this.string;
+  
+      for (var i = 0; i < sentence.length; i++) {
+  
+  
+          var num = alphabet.indexOf(sentence[i]);
+  
+          if (sentence[i] == alphabet[num]) {
+              alphabet = alphabet.replace(sentence[i], '') 
+          } 
+  
+      }
+  
+      if (alphabet.length == 0) {
+          return true;    
+      }
+      else {
+          return false;
+      }
+  
+  
+  }
+
 function App() {
   return (
     <div className="App">
