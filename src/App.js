93361c798a552cdb8179752ce4acc75247081f1c
Alexis K Grisham <alexisgrisham@AlexisGMac.local>
2019-06-18 20:14:13 -0400

    letterkata

--- src/App.js
+++ src/App.js
@@ -4,9 +4,32 @@ import './App.css';
 
 function allZeros (n) {
   var res = 0;
-  for(var i=5; i<n; i*=5)
+  for(var i=5; i<n; i*=)5
     res += Math.floor(n/i);
   return res;
+}
+
+function firstNonRepeatingLetter(s) {
+  var arr = s.split('');
+  var result = '';
+  var ctr = 0;
+ 
+  for (var x = 0; x < arr.length; x++) {
+    ctr = 0;
+ 
+    for (var y = 0; y < arr.length; y++) 
+    {
+      if (arr[x] === arr[y]) {
+        ctr+= 1;
+      }
+    }
+ 
+    if (ctr < 2) {
+      result = arr[x];
+      break;
+    }
+  }
+  return result;
 }
 
 
